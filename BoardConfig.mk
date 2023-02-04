b791ebeb478f3969ab3077614ddf9650507fa7df
greg <greg.jwaerhouse@gmail.com>
2023-02-04 06:04:40 -0500

    ****First release for afyonltecan lineage 18 build******

---
+++ BoardConfig.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common afyonlte
+include device/samsung/afyonlte-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/afyonltecan
+
+# Kernel
+#For Recovery
+#TARGET_KERNEL_CONFIG := kernel3.4.113_afyonltecan7_defconfig
+#For Normal build
+TARGET_KERNEL_CONFIG := kernel3.4.113_afyonltecan4_defconfig
+
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_afyonlte
+
+# NFC
+# include $(COMMON_PATH)/nfc/pn547/board.mk
+
+# Radio/RIL
+include $(COMMON_PATH)/radio/single/board.mk
+
+# inherit from the proprietary version
+include vendor/samsung/afyonltecan/BoardConfigVendor.mk
---
+++ init/init_afyonlte.cpp
@@ -0,0 +1,68 @@
+/*
+   Copyright (c) 2016, The Linux Foundation. All rights reserved.
+   Copyright (c) 2017-2020, The LineageOS Project. All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+
+#include "property_service.h"
+
+#include "init_msm8226.h"
+
+using android::base::GetProperty;
+
+void vendor_load_properties()
+{
+    std::string bootloader = GetProperty("ro.bootloader", "");
+
+    if (bootloader.find("G386T") == 0) {
+        /* afyonltetmo */
+        property_override("ro.build.description", "afyonltetmo-user 4.4.2 KOT49H G386TUVU1ANK3 release-keys");
+        set_ro_product_prop("device", "afyonltetmo");
+        set_ro_build_prop("fingerprint", "samsung/afyonltetmo/afyonltetmo:4.4.2/KOT49H/G386TUVU1ANK3:user/release-keys");
+        set_ro_product_prop("model", "SM-G386T");
+        set_ro_product_prop("name", "afyonltetmo");
+        gsm_properties("3", "0");
+    } else if (bootloader.find("G386W") == 0) {
+        /* afyonltecan */
+        property_override("ro.build.description", "afyonltevl-user 4.4.2 KOT49H G386WVLS1AQA1 release-keys");
+        set_ro_product_prop("device", "afyonltecan");
+        set_ro_build_prop("fingerprint", "samsung/afyonltevl/afyonltecan:4.4.2/KOT49H/G386WVLS1AQA1:user/release-keys");
+        set_ro_product_prop("model", "SM-G386W");
+        set_ro_product_prop("name", "afyonltecan");
+        gsm_properties("3", "0");
+    } else {
+        gsm_properties("3", "0");
+    }
+
+    std::string device = GetProperty("ro.product.device", "");
+    LOG(ERROR) << "Found bootloader id " << bootloader <<  " setting build properties for "
+        << device <<  " device" << std::endl;
+}
