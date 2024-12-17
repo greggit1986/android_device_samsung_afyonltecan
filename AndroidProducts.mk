fad68fc6b0ec6fe630fce421f685ca3b20cd9922
greg <greg.jwaerhouse@gmail.com>
2024-12-17 05:54:58 -0500

    ***Drop COMMON_LUNCH_CHOICES, A14 QPR2 shenanigans apparentlyadd .**

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,8 +15,3 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_afyonltecan.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_afyonltecan-user \
-    lineage_afyonltecan-userdebug \
-    lineage_afyonltecan-eng
