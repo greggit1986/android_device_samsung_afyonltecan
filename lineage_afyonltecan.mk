4f4d9f091cb85efecea99d699b5f1d663e8ed4b9
greg <greg.jwaerhouse@gmail.com>
2025-05-12 07:20:19 -0400

    lineage_afyonltecan.mk: Update device characteristics. Source: https://github.com/Gabboxl/android_device_samsung_gprimeltexx/tree/lineage-18.1

--- lineage_afyonltecan.mk
+++ lineage_afyonltecan.mk
@@ -20,6 +20,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/afyonltecan/full_afyonltecan.mk)
 
+# gprimeltexx-Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
+
+# gprimeltexx-Inherit from those products. Most specific first.
+PRODUCT_CHARACTERISTICS := phone
+
 PRODUCT_DEVICE := afyonltecan
 PRODUCT_NAME := lineage_afyonltecan
 
