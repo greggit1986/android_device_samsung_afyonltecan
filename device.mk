67076ce4521c5f3e02a0cbf962f43f98631e1331
greg <greg.jwaerhouse@gmail.com>
2025-06-01 17:51:38 -0400

    Get rid of libshim_imx175, not relevant for our device.

--- device.mk
+++ device.mk
@@ -20,10 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/afyonltecan/afyonltecan-vendor.mk)
 
-# Camera
-PRODUCT_PACKAGES += \
-    libshim_imx175
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
