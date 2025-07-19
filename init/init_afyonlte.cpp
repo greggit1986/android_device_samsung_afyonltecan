d26eacdda21331da879e594b0ad912b1efbe50d5
greg <greg.jwaerhouse@gmail.com>
2025-07-19 06:26:01 -0400

    Update fingerprints and build props for all variants according to latest firmware. Same values for SM-G386T1 are not yet confirmed to work...

--- init/init_afyonlte.cpp
+++ init/init_afyonlte.cpp
@@ -135,22 +135,22 @@ void vendor_load_properties()
     } else if (bootloader.find("G386T") == 0) {
         /* afyonltetmo and afyonlteMetroPCS */
         for (const auto &source : ro_product_props_default_source_order) {
-            set_ro_product_prop(source, "build.fingerprint", "samsung/afyonltetmo/afyonltetmo:4.4.2/KOT49H/G386TUVU1ANK3:user/release-keys");
+            set_ro_product_prop(source, "build.fingerprint", "samsung/afyonltetmo/afyonltetmo:4.4.2/KOT49H/G386TUVU1AQD2:user/release-keys");
             set_ro_product_prop(source, "device", "afyonltetmo");
             set_ro_product_prop(source, "model", "SM-G386T");
             set_ro_product_prop(source, "name", "afyonltetmo");
         }
-        property_override("ro.build.description", "afyonltetmo-user 4.4.2 KOT49H G386TUVU1ANK3 release-keys");
+        property_override("ro.build.description", "afyonltetmo-user 4.4.2 KOT49H G386TUVU1AQD2 release-keys");
         property_override("ro.build.product", "afyonltetmo");
     } else if (bootloader.find("G386T1") == 0) {
         /* afyonlteMetroPCS -- just in case something weird happens */
         for (const auto &source : ro_product_props_default_source_order) {
-            set_ro_product_prop(source, "build.fingerprint", "samsung/afyonltetmo/afyonltetmo:4.4.2/KOT49H/G386TUVU1ANK3:user/release-keys");
+            set_ro_product_prop(source, "build.fingerprint", "samsung/afyonltetmo/afyonltetmo:4.4.2/KOT49H/G386TUVU1AQD2:user/release-keys");
             set_ro_product_prop(source, "device", "afyonltetmo");
             set_ro_product_prop(source, "model", "SM-G386T");
             set_ro_product_prop(source, "name", "afyonltetmo");
         }
-        property_override("ro.build.description", "afyonltetmo-user 4.4.2 KOT49H G386TUVU1ANK3 release-keys");
+        property_override("ro.build.description", "afyonltetmo-user 4.4.2 KOT49H G386TUVU1AQD2 release-keys");
         property_override("ro.build.product", "afyonltetmo");
     }
 
